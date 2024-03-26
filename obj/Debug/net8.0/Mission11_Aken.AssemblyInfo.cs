8ce7ad2c5d2632766b7fcc3ee0d0bfaad78fdebc
elijahaken <elijahaken73@gmail.com>
2024-03-25 19:31:04 -0600

    Updated Title.

--- obj/Debug/net8.0/Mission11_Aken.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mission11_Aken.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Aken")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64825c368d3112294311e154c0f3b8579160876")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Aken")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Aken")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
